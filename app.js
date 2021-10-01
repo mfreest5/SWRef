bfd9e5934e489b405f55f176e8d62828419445b5
Mac Freestone <mac.freestone@gmail.com>
2021-09-30 21:21:43 -0600

    adding api to site

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 // setInterval 
-const error =  document.getElementById('error')
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyZj60I1A4yZSp2PYR_xi2o0Tn2p8ybZnzDXK8RGMYnHjK66eqg/exec'
-const form = document.querySelector('form')
-const feedback = document.getElementById('feedback')
+// being used on index.html
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if (feedback.value == "No") {
-        error.innerText = 'Please provide feedback'                
-    } else {
-        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => console.log('Success!', response))
-            .then(() => {
-                //This is where you write your code
-                document.querySelector('#email').value = ""
-                document.querySelector('#fname').value = ""
-                document.querySelector('#lname').value = ""
-                document.querySelector('#feedback').value = ""              
+const field1 = document.querySelector('#field1')
+const field2 = document.querySelector('#field2')
+const field3 = document.querySelector('#field3')
+const field4 = document.querySelector('#field4')
+const field5 = document.querySelector('#field5')
+const field6 = document.querySelector('#field6')
+const field7 = document.querySelector('#field7')
+const field8 = document.querySelector('#field8')
+const field9 = document.querySelector('#field9')
 
+const swrefRequest = (event) => {
+    event.preventDefault()
+    const swref = document.querySelector('input[name="search"]').value
+    const urlString = `https://swapi.dev/api/people/?search=${swref}`
+    console.log(swref)
 
+    fetch(urlString)
+        .then(res => res.json())
+        .then(res => {
+            console.log(swref)
+            console.log(res.results[0])
+            field1.innerText = res.results[0].name
+            field2.innerText = `Height: ${res.results[0].height}`
+            field3.innerText = `Hair Color: ${res.results[0].hair_color}`
+            field4.innerText = `Birth Year: ${res.results[0].birth_year}`
+            field5.innerText = `Eye Color: ${res.results[0].eye_color}`
+            field7.innerText = `Gender: ${res.results[0].gender}`
+            // field8.innerText = res.results[0].name
+            // field9.innerText = res.results[0].name
 
+            console.log(res.results[0].homeworld)
+            fetch(res.results[0].homeworld)
+                .then(res => res.json())
+                .then( res => {
+                    field6.innerText = `Homeworld: ${res.name}`
+                    console.log(res)
+                })
 
-            //Do not alter below this line
         })
-        .catch(error => console.error('Error!', error.message))
-    }
-})
+    
+}
+
+const referendum = document.querySelector("#referendum")
+
+referendum.addEventListener("submit", swrefRequest);
+
---
+++ app2.js
@@ -0,0 +1,28 @@
+const error =  document.getElementById('error')
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyZj60I1A4yZSp2PYR_xi2o0Tn2p8ybZnzDXK8RGMYnHjK66eqg/exec'
+const form = document.querySelector('form')
+const feedback = document.getElementById('feedback')
+
+// feedback is using this event listener
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (feedback.value == "No") {
+        error.innerText = 'Please provide feedback'                
+    } else {
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => console.log('Success!', response))
+            .then(() => {
+                //This is where you write your code
+                document.querySelector('#email').value = ""
+                document.querySelector('#fname').value = ""
+                document.querySelector('#lname').value = ""
+                document.querySelector('#feedback').value = ""              
+
+
+
+
+            //Do not alter below this line
+        })
+        .catch(error => console.error('Error!', error.message))
+    }
+})
