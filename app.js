2ef71f09a08436c28bde06295150f2e34b415a91
Mac Freestone <mac.freestone@gmail.com>
2021-10-05 18:23:04 -0600

    cleanup and adding more characters

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const leia = document.querySelector('#leia')
 const obi = document.querySelector('#obi')
 const luke = document.querySelector('#luke')
 const boba = document.querySelector('#boba')
+const darth = document.querySelector('#darth')
+const pal = document.querySelector('#pal')
+const r2 = document.querySelector('#r2')
+const c3po = document.querySelector('#c3po')
 
 const swrefRequest = (event) => {
     event.preventDefault()
@@ -224,6 +228,121 @@ const chewRequest = (event) => {
     
 }
 
+const darthRequest = (event) => {
+    event.preventDefault()
+    const urlString = `https://swapi.dev/api/people/?search=vader`
+
+    fetch(urlString)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.results[0])
+            field1.innerText = res.results[0].name
+            field2.innerText = `Height: ${res.results[0].height}`
+            field3.innerText = `Hair Color: ${res.results[0].hair_color}`
+            field4.innerText = `Birth Year: ${res.results[0].birth_year}`
+            field5.innerText = `Eye Color: ${res.results[0].eye_color}`
+            field7.innerText = `Gender: ${res.results[0].gender}`
+            // field8.innerText = res.results[0].name
+            // field9.innerText = res.results[0].name
+
+            console.log(res.results[0].homeworld)
+            fetch(res.results[0].homeworld)
+                .then(res => res.json())
+                .then( res => {
+                    field6.innerText = `Homeworld: ${res.name}`
+                    console.log(res)
+                })
+
+        })
+    
+}
+
+const palRequest = (event) => {
+    event.preventDefault()
+    const urlString = `https://swapi.dev/api/people/?search=pal`
+
+    fetch(urlString)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.results[0])
+            field1.innerText = res.results[0].name
+            field2.innerText = `Height: ${res.results[0].height}`
+            field3.innerText = `Hair Color: ${res.results[0].hair_color}`
+            field4.innerText = `Birth Year: ${res.results[0].birth_year}`
+            field5.innerText = `Eye Color: ${res.results[0].eye_color}`
+            field7.innerText = `Gender: ${res.results[0].gender}`
+            // field8.innerText = res.results[0].name
+            // field9.innerText = res.results[0].name
+
+            console.log(res.results[0].homeworld)
+            fetch(res.results[0].homeworld)
+                .then(res => res.json())
+                .then( res => {
+                    field6.innerText = `Homeworld: ${res.name}`
+                    console.log(res)
+                })
+
+        })
+    
+}
+
+const r2Request = (event) => {
+    event.preventDefault()
+    const urlString = `https://swapi.dev/api/people/?search=r2`
+
+    fetch(urlString)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.results[0])
+            field1.innerText = res.results[0].name
+            field2.innerText = `Height: ${res.results[0].height}`
+            field3.innerText = `Hair Color: ${res.results[0].hair_color}`
+            field4.innerText = `Birth Year: ${res.results[0].birth_year}`
+            field5.innerText = `Eye Color: ${res.results[0].eye_color}`
+            field7.innerText = `Gender: ${res.results[0].gender}`
+            // field8.innerText = res.results[0].name
+            // field9.innerText = res.results[0].name
+
+            console.log(res.results[0].homeworld)
+            fetch(res.results[0].homeworld)
+                .then(res => res.json())
+                .then( res => {
+                    field6.innerText = `Homeworld: ${res.name}`
+                    console.log(res)
+                })
+
+        })
+    
+}
+
+const c3poRequest = (event) => {
+    event.preventDefault()
+    const urlString = `https://swapi.dev/api/people/?search=3po`
+
+    fetch(urlString)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.results[0])
+            field1.innerText = res.results[0].name
+            field2.innerText = `Height: ${res.results[0].height}`
+            field3.innerText = `Hair Color: ${res.results[0].hair_color}`
+            field4.innerText = `Birth Year: ${res.results[0].birth_year}`
+            field5.innerText = `Eye Color: ${res.results[0].eye_color}`
+            field7.innerText = `Gender: ${res.results[0].gender}`
+            // field8.innerText = res.results[0].name
+            // field9.innerText = res.results[0].name
+
+            console.log(res.results[0].homeworld)
+            fetch(res.results[0].homeworld)
+                .then(res => res.json())
+                .then( res => {
+                    field6.innerText = `Homeworld: ${res.name}`
+                    console.log(res)
+                })
+
+        })
+    
+}
 const referendum = document.querySelector("#referendum")
 
 const beings = document.getElementsByClassName("beings")
@@ -234,6 +353,10 @@ chew.addEventListener('click', chewRequest)
 boba.addEventListener('click', bobaRequest)
 luke.addEventListener('click', lukeRequest)
 obi.addEventListener('click', obiRequest)
+darth.addEventListener('click', darthRequest)
+pal.addEventListener('click', palRequest)
+r2.addEventListener('click', r2Request)
+c3po.addEventListener('click', c3poRequest)
 
 referendum.addEventListener("submit", swrefRequest);
 
