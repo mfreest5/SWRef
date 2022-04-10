911ce3189ab32313cc5b4df0e49ba1f6ea594ac2
Mac Freestone <mac.freestone@gmail.com>
2022-04-10 16:54:07 -0600

    adding some more cypress tests

--- cypress/integration/1-getting-started/SWRef_Test.spec.js
+++ cypress/integration/1-getting-started/SWRef_Test.spec.js
@@ -7,6 +7,33 @@ describe('Verifying homepage information', () =>  {
         cy.title().should('eq', 'Macs Star Wars Referendum')
 
     })
+
+    it('Displays boba information after clicking', () => {
+        cy.get('#boba').click()
+        cy.get('#field1').should('be.visible')
+        cy.get('#field1').should('contain', 'Boba Fett')
+
+    })
+
+    it('Redirects to the feedback page after selecting Give Feedback', () => {
+        cy.get('[href="./feedback.html"]').should('exist').click()
+        cy.url().should('contain', 'http://127.0.0.1:5501/feedback')
+
+    })
+
+    it('Redirects to the feedback page after selecting Give About Me', () => {
+        cy.get('[href="./aboutme.html"]').should('exist').click()
+        cy.log('We redirected')
+        // cy.url().should('contain', 'http://127.0.0.1:5501/aboutme')
+
+    })
+
+    it('Redirects to the feedback page after selecting Give About Me', () => {
+        cy.get('[href="https://www.starwars.com/databank"]').should('exist').click()
+        cy.log('We redirected')
+        cy.url().should('contain', 'https://www.starwars.com/databank')
+
+    })
 })
 
 
